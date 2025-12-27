6e6354ff9e23beae29d08deafeb76b916ce38e03
Gianfranco <cicciomesere@gmail.com>
2025-12-27 00:15:17 +0000

    Improved compile bat

--- compile.bat
+++ compile.bat
@@ -1,31 +1,39 @@
 @echo off
 set ProjName=standalone_template
+set IwadOnly=n
 echo Ready to Start.
 
-echo Step 1: Unpacking iwad for merging
+echo Step 1: Clean up previous release
+echo Cleaning up...
+rd /s /q build
+mkdir build
+
+echo Step 2: Unpacking iwad for merging
 echo Unpacking...
-7z\7za.exe x %ProjName%.ipk3 -o build\data -y
+7z\7za.exe x %ProjName%.ipk3 -obuild\data -y
 echo Done unpacking.
 
-echo Step 2: Merge unpacked iwad with data
+echo Step 3: Merge unpacked iwad with data
 echo Merging...
 xcopy /E /I data\* build\data\
 
-echo Step 3: Repack everything
+echo Step 4: Repack everything
 echo Packing...
-7z\7za.exe a -r -ssw -tzip build\%ProjName%n.ipk3 build\data\*
+cd build\data
+..\..\7z\7za.exe a -r -ssw -tzip ..\%ProjName%.ipk3 "*"
 echo Done packing.
 
-echo Step 4: Replace old version
-echo Replacing old version.
-cd build
-del %ProjName%.ipk3
-ren %ProjName%n.ipk3 %ProjName%.ipk3
-echo Done replacing.
+echo Step 5: Clean up unpacked data
+echo Cleaning up unpacked data...
+cd ..
+rd /s /q data
 
-echo Step 5: Clean up
-echo Cleaning up...
-rd /S /Q data
+if NOT "%IwadOnly%"=="y" (
+    echo Step 6: Move ZDoom binaries to build folder
+    echo Moving UZDoom binaries...
+    cd ..
+    xcopy bin\* build\
+)
 
 echo Done.
 pause
